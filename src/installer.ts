b83c236cbe8a864ae76175db66ee7c208d5a8820
Alexande B <abobrikovich@gmail.com>
2020-05-10 00:14:47 +0300

    Fix format issues

--- src/installer.ts
+++ src/installer.ts
@@ -7,23 +7,29 @@ const IS_WINDOWS = process.platform === 'win32';
 const IS_MACOSX = process.platform === 'darwin';
 const IS_LINUX = process.platform === 'linux';
 
-export async function installMetanormaVersion(version: string) {
-  const installDir: string | null = tc.find('Ruby', version);
-
-  if (!installDir) {
-    throw new Error(`Version ${version} not found`);
-  }
-
-  const toolPath: string = path.join(installDir, 'bin');
-  const version: string = 'master';
+export async function installMetanormaVersion(version: string | null) {
+  let toolPath: string = '/usr/bin/metanorma';
 
   if (IS_MACOSX) {
-    const formulaUrl: string = `https://raw.githubusercontent.com/metanorma/homebrew-metanorma/${version}/Formula/metanorma.rb`
+    let formulaUrl: string = `https://raw.githubusercontent.com/metanorma/homebrew-metanorma/v${version}/Formula/metanorma.rb`;
+    if (version == null) {
+      formulaUrl = `https://raw.githubusercontent.com/metanorma/homebrew-metanorma/master/Formula/metanorma.rb`;
+    }
     exec.exec(`brew install --HEAD ${formulaUrl}`);
+    toolPath = '/usr/local/bin/metanorma';
   } else if (IS_LINUX) {
-    exec.exec(`curl -L https://raw.githubusercontent.com/metanorma/metanorma-linux-setup/master/ubuntu.sh | sudo bash`);
+    // TODO support version
+    exec.exec(
+      `curl -L https://raw.githubusercontent.com/metanorma/metanorma-linux-setup/master/ubuntu.sh | sudo bash`
+    );
+    toolPath = '/usr/bin/metanorma';
   } else if (IS_WINDOWS) {
-    exec.exec(`choco install -y metanorma`);
+    if (version == null) {
+      exec.exec('choco install -y metanorma');
+    } else {
+      exec.exec(`choco install -y metanorma --version ${version}`);
+    }
+    toolPath = `${process.env.ChocolateyPackageFolder}\\metanorma`;
   }
 
   core.addPath(toolPath);
