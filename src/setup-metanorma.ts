7a6255d86abdd8c08cddaf349919f8ddc30718a5
Alexande B <abobrikovich@gmail.com>
2022-01-10 09:02:51 +0100

    #5 support different metanorma versions to install

--- src/setup-metanorma.ts
+++ src/setup-metanorma.ts
@@ -4,7 +4,9 @@ import {installMetanormaVersion} from './installer';
 async function run() {
   try {
     let version = core.getInput('version');
-    await installMetanormaVersion(version);
+    let snap_channel = core.getInput('snap-channel');
+    let choco_prerelase = core.getInput('choco-prerelase') === 'true';
+    await installMetanormaVersion(version, snap_channel, choco_prerelase);
   } catch (error) {
     core.setFailed(error.message);
   }
